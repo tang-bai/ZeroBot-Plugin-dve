20fc582eca0aeac823f17f6a122c9eb95ffeb9bc
tang-guo-guo <71223830+tang-guo-guo@users.noreply.github.com>
2022-09-13 13:03:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -184,7 +184,7 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:8080", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "椛椛", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
@@ -215,8 +215,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 1249922508)
+	 sus = append(sus, 1965880347)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
